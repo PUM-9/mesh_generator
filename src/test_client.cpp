ba1a5e9d0e3c68cd4ab0aa4497c26a1d3877d314
ehmanx <ehmanx@gmail.com>
2017-02-23 14:44:06 +0100

    added comments

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -1,3 +1,6 @@
+//
+// A client that calls the mesh service to test it
+//
 #include "ros/ros.h"
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
@@ -9,29 +12,29 @@ typedef mesh_generator::MeshPointCloud MeshPointCloud;
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "mesh_generator_test_client");
+    ros::init(argc, argv, "mesh_generator_test_client"); //inits the node
 
     ros::NodeHandle node_handle;
     ros::ServiceClient client = node_handle.serviceClient<mesh_generator::MeshPointCloud>("mesh_point_cloud");
 
     mesh_generator::MeshPointCloud srv;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>); //allocates memory to a new point cloud
     pcl::PCLPointCloud2 cloud_blob;
-    if (pcl::io::loadPCDFile("bun0.pcd", cloud_blob) == -1) {
+    if (pcl::io::loadPCDFile("bun0.pcd", cloud_blob) == -1) { // loads the file bun0.pcd into the  a tmp point cloud
         std::cout << "Couldn't read file bun0.pcd" << std::endl;
     }
-    pcl::fromPCLPointCloud2(cloud_blob, *cloud);
+    pcl::fromPCLPointCloud2(cloud_blob, *cloud); // saves the tmp point cloud cloud_blob as the point cloud cloud
 
     sensor_msgs::PointCloud2 ros_cloud;
-    pcl::toROSMsg(*cloud, ros_cloud);
+    pcl::toROSMsg(*cloud, ros_cloud); // converts the point cloud from PCL to ROS
     srv.request.point_cloud = ros_cloud;
 
     if (client.call(srv)) {
         std::cout << "Exit code: " << srv.response.exit_code << std::endl;
     }
     else {
-        ROS_ERROR("Failed to call service add_two_ints");
+        ROS_ERROR("Failed to call service mesh_point_cloud");
         return 1;
     }
 
