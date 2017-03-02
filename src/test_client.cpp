f9cb44a4519271349b2f87801708e840d3522b96
Martin <martin95lundberg@gmail.com>
2017-03-02 11:56:34 +0100

    Added test of 3 files in test_client

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -7,7 +7,7 @@
 #include <pcl/io/vtk_lib_io.h>
 #include <pcl_ros/point_cloud.h>
 #include "mesh_generator/MeshPointCloud.h"
-
+#include <sstream>
 
 int main(int argc, char **argv)
 {
@@ -18,29 +18,54 @@ int main(int argc, char **argv)
 
     mesh_generator::MeshPointCloud srv;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_lamp(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cat(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_wolf(new pcl::PointCloud<pcl::PointXYZ>);
+    
     pcl::PCLPointCloud2 cloud_blob;
-    if (pcl::io::loadPCDFile("bun0.pcd", cloud_blob) == -1) {
+    if (pcl::io::loadPCDFile("lamppost.pcd", cloud_blob) == -1) {
         std::cout << "Couldn't read file bun0.pcd" << std::endl;
     }
-    pcl::fromPCLPointCloud2(cloud_blob, *cloud);
+    pcl::fromPCLPointCloud2(cloud_blob, *cloud_lamp);
 
+    if (pcl::io::loadPCDFile("ism_test_cat.pcd", cloud_blob) == -1) {
+        std::cout << "Couldn't read file bun0.pcd" << std::endl;
+    }
+    pcl::fromPCLPointCloud2(cloud_blob, *cloud_cat);
+
+    if (pcl::io::loadPCDFile("ism_test_wolf.pcd", cloud_blob) == -1) {
+        std::cout << "Couldn't read file bun0.pcd" << std::endl;
+    }
+    pcl::fromPCLPointCloud2(cloud_blob, *cloud_wolf);
+
+    
     // Convert point cloud to a ROS message and send it in request
     sensor_msgs::PointCloud2 ros_cloud;
-    pcl::toROSMsg(*cloud, ros_cloud);
+    pcl::toROSMsg(*cloud_lamp, ros_cloud);
     srv.request.point_cloud = ros_cloud;
 
-    if (client.call(srv)) {
-        pcl::PolygonMesh mesh;
-        pcl_conversions::toPCL(srv.response.mesh, mesh);
-	
-        if (pcl::io::savePolygonFile("mesh-response.stl", mesh)) {
-	        ROS_INFO("Saved file mesh-response.stl");
-        }
-    }
-    else {
-        ROS_ERROR("Failed to call service mesh_point_cloud");
-        return 1;
+    for (size_t i = 0; i < 3; ++i) {
+        if (client.call(srv)) {
+	    pcl::PolygonMesh mesh;
+	    pcl_conversions::toPCL(srv.response.mesh, mesh);
+
+	    std::stringstream file_name;
+	    file_name << "mesh-response" << i << ".stl";
+	    if (pcl::io::savePolygonFile(file_name.str(), mesh)) {
+	        ROS_INFO("Saved file mesh-responseX.stl");
+	    }
+
+	    if (i == 0) {
+	        pcl::toROSMsg(*cloud_cat, ros_cloud);
+		srv.request.point_cloud = ros_cloud;
+	    } else if (i == 1) {
+	        pcl::toROSMsg(*cloud_wolf, ros_cloud);
+		srv.request.point_cloud = ros_cloud;
+	    }
+	} else {
+	    ROS_ERROR("Failed to call service mesh_point_cloud");
+            return 1;
+	}
     }
 
     return 0;
